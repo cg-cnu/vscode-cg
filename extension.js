d2a7a691265434e35fb6c581d53854e95debd15b
sreenivas alapati <sreenivas9alapati@gmail.com>
2016-07-22 17:37:59 +0530

    refactoring stuff

--- extension.js
+++ extension.js
@@ -5,16 +5,17 @@ function activate(context) {
 
     console.log('Congratulations, your extension "code-cg" is now active!');
 
-    var sendCmd = function (HOST, PORT, COMMAND, callback) {
+    var sendCommand = function ( port, command, callback) {
 
-        client = net.connect({ port: PORT, host: HOST }, function () {
-            console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-            client.write('\n ' + COMMAND + ' \n');
+        var host = vscode.workspace.getConfiguration('cg')['host'];
+
+        client = net.connect({ port: port, host: host }, function () {
+            console.log('CONNECTED TO: ' + host + ':' + port);
+            client.write('\n ' + command + ' \n');
         });
 
         // on success
         client.on('data', function (data) {
-            console.log('DATA: ' + data);
             // Close the client socket completely: end / destroy ?
             client.destroy();
             callback(null, data);
@@ -26,8 +27,10 @@ function activate(context) {
         })
     }
 
+    // Given the languageId returns the selected or whole code
+    var getData = function (languageId) {
 
-    var getCode = function (languageId) {
+        console.log("entered get Text")
 
         var editor = vscode.window.activeTextEditor;
         if (!editor) {
@@ -36,65 +39,104 @@ function activate(context) {
         }
 
         var doc = editor.document;
-
+        // console.log( "doc.filePath" );
+        // console.log( editor.filePath );
+        // console.log( "doc.filePath" );
+        // console.log( doc.languageId );
         // Only if its a valid file; which means its also saved;
         if (doc.languageId !== languageId) {
-            vscode.window.showWarningMessage('Not a valid file!');
-            return;
+            // vscode.window.showWarningMessage('Not a valid file!');
+            console.log("invalid language");
+            return "invalid language";
         }
 
-        // warn if the file is diry
+        // warn if the file is dirty
         if (doc.isDirty) {
+            console.log("very dirty file")
             vscode.window.showWarningMessage('Save the file!');
             return;
         }
 
         var selText = doc.getText(editor.selection);
+        console.log("selText: " + selText);
+        var docText = doc.getText();
+        console.log("docText: " + docText);
+
+        var data = {
+            "selText": doc.getText(editor.selection),
+            "docText": doc.getText()
+            // "filePath": doc.filePath
+        }
 
         //  if anything selected;
         // TODO: simplify this one
-        if (selText.length !== 0) {
-            var text = selText;
-            // if nothing selected
-        } else {
-            // else get all the text in the document
-            var docText = doc.getText();
-            // if the file is empty
-            // TODO: better comparision find out what exactly does docText is
-            if (docText === "") {
-                vscode.window.showWarningMessage('No text in the file!');
-                return null;
-            } else {
-                var text = docText
-                return text;
-            }
-        };
+        // if (selText.length !== 0) {
+        //     return  {"selCode": selText,
+        //             "filePath": null}
+        //     // if nothing selected
+        // } else {
+        //     // else get all the text in the document
+        //     var docText = doc.getText();
+        //     // if the file is empty
+        //     // TODO: better comparision find out what exactly does docText is
+        //     if (docText === "") {
+        //         // vscode.window.showWarningMessage('No text in the file!');
+        //         return null;
+        //     } else {
+        //         // var text = docText
+        //         return {"fullCode": docText,
+        //                 "filePath" : filePath }
+        //         // return text;
+        //     }
+        // }
+        return data;
     }
 
     // maya: python - mel
     var sendToMaya = vscode.commands.registerCommand('extension.sendToMaya', function () {
-        // The code you place here will be executed every time your command is executed
 
-        // read the values from the config file
-        var HOST = vscode.workspace.getConfiguration('cg')['host'];
-        var PORT = vscode.workspace.getConfiguration('maya')['python-port'];
-        // get the python code from the current file
-        // either selected or all
+        var software = 'maya';
+        var languages = vscode.workspace.getConfiguration('maya')['languages'];
+        // console.log(languages);
 
-        var COMMAND, PORT = getCode('python');
+        for (var languageId in languages) {
+            var data = getData(languageId)
+            console.log("data: " + data);
+            var port = languages[languageId];
+            console.log("port: " + port)
+            if (data === "invalid language"){
+                console.log("No valid language found!");
+                return;
+            }else if (data.selText.length === 0 ) {
+                var command = data.selText;
+            }else{
+                var command = data.docText;
+                console.log("found command");
+                console.log(command);
+            }
 
-        // try for mel ?
-
-        if (COMMAND) {
-            sendCmd(HOST, PORT, COMMAND, function (err, success) {
-                console.log(err);
-                console.log(success);
-                if (!err) {
-                    vscode.window.showInformationMessage('Executed in Maya!');
-                } else {
-                    vscode.window.showWarningMessage('Failed to execute in Maya!');
-                }
-            });
+        sendCommand( port, command, function (err, data) {
+            console.log("error: " + err);
+            console.log("data: " + data);
+            if (!err) {
+                vscode.window.showInformationMessage('Executed in Maya!');
+            } else {
+                vscode.window.showWarningMessage('Failed to execute in Maya!');
+            }
+        }
+ )
+            // if (code !== "invalid language") {
+            //     var port = languages[languageId];
+            //     sendCommand(software, code, port, function (err, success) {
+            //         console.log(err);
+            //         console.log(success);
+            //         if (!err) {
+            //             vscode.window.showInformationMessage('Executed in Maya!');
+            //         } else {
+            //             vscode.window.showWarningMessage('Failed to execute in Maya!');
+            //         }
+            //     });
+            // }
         }
     });
 
@@ -103,16 +145,14 @@ function activate(context) {
     // mari: python
     var sendToMari = vscode.commands.registerCommand('extension.sendToMari', function () {
         // The code you place here will be executed every time your command is executed
-
         // read the values from a different config file
         // if the config file is not present make this as the default
         var HOST = vscode.workspace.getConfiguration('cg')['host'];
-        var PORT = vscode.workspace.getConfiguration('mari')['port'];
+        // var PORT = vscode.workspace.getConfiguration('mari')['port'];
         var COMMAND = getPyCode()
-
         if (COMMAND) {
             COMMAND += " \x04"
-            sendCmd(HOST, PORT, COMMAND, function (err, success) {
+            sendCommand(HOST, PORT, COMMAND, function (err, success) {
                 if (!err) {
                     vscode.window.showInformationMessage('Executed in Mari!');
                 } else {
@@ -121,9 +161,7 @@ function activate(context) {
             });
         }
     });
-
     context.subscriptions.push(sendToMari);
-
 }
 exports.activate = activate;
 
